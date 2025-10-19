c3e44c1293c7dab55e16033a57c4664d16b9f8a0
vaka <vaka@vaka.pro>
2025-10-19 21:31:01 +0300

    feat: adds input and handles which control input.value

---
+++ src/components/Input/Input.tsx
@@ -0,0 +1,29 @@
+import { useState, type ChangeEvent, type FC } from "react";
+
+import { cnInput } from "./Input.classname";
+
+import './Input.css';
+
+type InputProps = {
+    limit: number;
+    onChange: (value: string) => void;
+}
+
+const Input: FC<InputProps> = ({ limit, onChange }) => {
+    const [inputText, setInputText] = useState('');
+
+    const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+        const inputValue = event.target.value;
+        onChange(inputValue);
+        setInputText(inputValue);
+    }
+
+    return <input
+        className={cnInput()}
+        value={inputText}
+        onChange={handleChange}
+        maxLength={limit}>
+    </input>
+}
+
+export { Input };
